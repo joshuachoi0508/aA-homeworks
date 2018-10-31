d3c485294e3818e5990587a18db204e1ee37f5e7
Josh Choi <joshuachoi0508@gmail.com>
2018-10-30 22:05:30 -0700

    Complete rspec homework

--- W2D2/lib/board.rb
+++ W2D2/lib/board.rb
@@ -26,40 +26,46 @@ class Board
   end
 
   def valid_move?(start_pos)
-    raise "Invalid starting cup" if start_pos > 14
+    raise "Invalid starting cup" if start_pos > 13
     raise "Starting cup is empty" if self.cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
-    movement = self.cups[start_pos].length
+    # empties cup
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
 
-    self.cups[start_pos] = []
-
-    i = start_pos + 1
-    while movement > 0
-      if i > 14
-        i = 0
-      end
-
-      if @player1 == current_player_name && i == 13
-        i += 1
-        next
-      elsif @player2 == current_player_name && i == 6
-        i += 1
-        next
+    # distributes stones
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      # places stones in the correct current player's cups
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
       else
-        
-        self.cups[i] << :stone
-        i += 1
-        movement -= 1
+        @cups[cup_idx] << stones.pop
       end
     end
+
     render
-    next_turn(i)
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
+      :switch
+    else
+      # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
+    end
   end
 
   def render
@@ -70,9 +76,14 @@ class Board
     puts ""
   end
 
-  def one_side_empty?
-  end
-
   def winner
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      :draw
+    else
+      player1_count > player2_count ? @name1 : @name2
+    end
   end
 end
---
+++ W2D3/rspec_homework/spec/dessert_spec.rb
@@ -0,0 +1,69 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  subject(:dessert) { Dessert.new("cake", 20, chef) }
+  let(:chef) { double("chef") }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("cake")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(20)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "10", chef) }.to raise_error
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      expect(dessert.add_ingredient('flour')).to include('flour')
+    end
+  end
+
+  describe "#mix!" do
+    before(:each) { dessert.add_ingredient('chocolate') }
+
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient('flour')
+      dessert.add_ingredient('egg')
+      expect(dessert.mix!).to_not eq(['chocolate', 'flour', 'egg'])
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      expect(dessert.eat(1)).to eq(19)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{ dessert.eat(100) }.to raise_error
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Choi")
+      expect(dessert.serve).to eq("Chef Choi has made 20 cakes!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
+  end
+end
