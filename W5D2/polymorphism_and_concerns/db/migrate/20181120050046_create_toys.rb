7cc18800188bfcd6c2b4aaed35e3ab33d1d64642
Josh Choi <joshuachoi0508@gmail.com>
2018-11-19 21:04:26 -0800

    Create toys table

---
+++ W5D2/polymorphism_and_concerns/db/migrate/20181120050046_create_toys.rb
@@ -0,0 +1,9 @@
+class CreateToys < ActiveRecord::Migration[5.1]
+  def change
+    create_table :toys do |t|
+      t.string :name, null: false
+      t.references :toyable, polymorphic: true, index: true
+    end
+    add_index :toys, :name, unique: true
+  end
+end
