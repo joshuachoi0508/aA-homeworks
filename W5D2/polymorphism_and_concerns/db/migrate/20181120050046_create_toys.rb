6aa59581257f2e831d29a812d31c1de1efe0d066
Josh Choi <joshuachoi0508@gmail.com>
2018-11-19 21:09:47 -0800

    Built associations in corgi, cat, and toy classes

--- W5D2/polymorphism_and_concerns/db/migrate/20181120050046_create_toys.rb
+++ W5D2/polymorphism_and_concerns/db/migrate/20181120050046_create_toys.rb
@@ -4,6 +4,6 @@ class CreateToys < ActiveRecord::Migration[5.1]
       t.string :name, null: false
       t.references :toyable, polymorphic: true, index: true
     end
-    add_index :toys, :name, unique: true
+    add_index :toys, [:name, :toyable_id, :toyable_type], unique: true
   end
 end
