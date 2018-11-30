8e8a11020df1c5b2a5464d8fd2b295d71d747f18
Josh Choi <joshuachoi0508@gmail.com>
2018-11-29 21:34:42 -0800

    Run webpack

--- W6D5/node_package_manager/webpack.config.js
+++ W6D5/node_package_manager/webpack.config.js
@@ -1,10 +1,23 @@
 var path = require('path');
 
 module.exports = {
-  entry: "/etry.jsx",
+  entry: './entry.jsx',
   output: {
-    path: path.resolve(__dirname),
-    filename: "bundle.js"
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
   },
 
   devtool: 'source-map',
