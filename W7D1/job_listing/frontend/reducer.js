55eb1af09d3c32336af882d1537278feb498d6c4
Josh Choi <joshuachoi0508@gmail.com>
2018-12-02 17:44:38 -0800

    Create reducer

--- W7D1/job_listing/frontend/reducer.js
+++ W7D1/job_listing/frontend/reducer.js
@@ -1,10 +1,18 @@
 const initialState = {
-  city: "Please Select", 
+  city: "Please Select",
   jobs: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type) {
+    case "SWITCH_LOCATION":
+    return {
+      city: action.city,
+      jobs: action.jobs
+    };
+    default:
+      return state;
+  } // remove this and fill out the body of the reducer function
 };
 
 export default reducer;
